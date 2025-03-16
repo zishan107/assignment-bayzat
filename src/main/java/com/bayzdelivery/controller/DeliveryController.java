f7df5fc4b4d08b4ff6fd89dce9e6b3c855298e35
zishan107 <zishanjaved74@gmail.com>
2025-03-17 01:28:59 +0530

    Update DeliveryController.java

--- src/main/java/com/bayzdelivery/controller/DeliveryController.java
+++ src/main/java/com/bayzdelivery/controller/DeliveryController.java
@@ -1,14 +1,22 @@
 package com.bayzdelivery.controller;
 
 import com.bayzdelivery.model.Delivery;
+import com.bayzdelivery.model.Person;
+import com.bayzdelivery.repositories.DeliveryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.bayzdelivery.service.DeliveryService;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class DeliveryController {
